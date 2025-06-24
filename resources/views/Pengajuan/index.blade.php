336ad3312b121b6bb9f44a266a2425a59688c5c4
Biila <nabiilahaulasafdaa@gmail.com>
2025-06-24 15:56:41 +0700

    tampilan dan proses login

---
+++ resources/views/Pengajuan/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.template')
+
+@section('title')
+    Data Pengajuan
+@endsection
+
+@section('headline')
+    Data Pengajuan
+@endsection
+
+@section('content')
+    Data Pengajuan
+@endsection
+
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,95 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Responsive Admin &amp; Dashboard Template based on Bootstrap 5">
+	<meta name="author" content="AdminKit">
+	<meta name="keywords" content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
+
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	{{-- <link rel="shortcut icon" href={{ asset ('img/icons/icon-48x48.png') }}/> --}}
+
+	<link rel="canonical" href="https://demo-basic.adminkit.io/pages-sign-in.html" />
+
+	<title>Sign In | AdminKit Demo</title>
+
+	<link href={{ asset ('css/app.css') }} rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+</head>
+
+<body>
+	<main class="d-flex w-100">
+		<div class="container d-flex flex-column">
+			<div class="row vh-100">
+				<div class="col-sm-10 col-md-8 col-lg-6 col-xl-5 mx-auto d-table h-100">
+					<div class="d-table-cell align-middle">
+
+						<div class="text-center mt-4">
+							<h1 class="h2">Welcome back!</h1>
+							<p class="lead">
+								Sign in to your account to continue
+							</p>
+						</div>
+
+						<div class="card">
+							<div class="card-body">
+								<div class="m-sm-3">
+									<form method="POST" action="{{ route('login') }}">
+                                        @csrf
+										<div class="mb-3">
+											<label class="form-label">Email</label>
+											<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Enter your email">
+                                                @error('email')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+										</div>
+										<div class="mb-3">
+											<label class="form-label">Password</label>
+											<input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="Enter your password">
+                                                @error('password')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+										</div>
+										<div>
+											<div class="form-check align-items-center">
+												<input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                                <label class="form-check-label" for="remember">
+                                                    {{ __('Remember Me') }}
+                                                </label>
+											</div>
+										</div>
+										<div class="d-grid gap-2 mt-3">
+											<button type="submit" class="btn btn-primary">
+                                                {{ __('login') }}
+                                            </button>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+						<div class="text-center mb-3">
+							 <a href="pages-sign-up.html">Forgot password?</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+
+	<script src={{ asset ('js/app.js') }}></script>
+
+</body>
+
+</html>
+
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -70,4 +161,4 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
