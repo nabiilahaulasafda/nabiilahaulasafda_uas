7d56c90bf08c2367f4ff988a283f13792ebf0f5b
Biila <nabiilahaulasafdaa@gmail.com>
2025-06-24 14:34:58 +0700

    proses logout

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -236,7 +236,15 @@
 								<a class="dropdown-item" href="index.html"><i class="align-middle me-1" data-feather="settings"></i> Settings & Privacy</a>
 								<a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="help-circle"></i> Help Center</a>
 								<div class="dropdown-divider"></div>
-								<a class="dropdown-item" href="#">Log out</a>
+								<a class="dropdown-item" href="{{ route('logout') }}"Add commentMore actions
+                                    onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                    {{ __('logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
 							</div>
 						</li>
 					</ul>
